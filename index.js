f629feb7737614400a48aaa3fe1047fc35ea9bd2
AquIce <timeo.perrinjaquet@gmail.com>
2023-07-11 12:13:11 +0200

    Added moves

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const { PIECES, Color, Piece, PieceType, Filter, Flags, CheckMove, SelectPiece } = require('./pce.js')
+const {
+	PIECES,
+	Color,
+	Piece,
+	PieceType,
+	Rank,
+	File, Filter,
+	Flags,
+	CheckMove,
+	SelectPiece,
+	Board,
+	GetPiece,
+	Coords,
+	Move,
+} = require('./pce.js')
 
 /* console.log(Filter(PieceType.PAWN, [Flags.CAPTURE, Flags.FIRST_PAWN])(2, 0))
 console.log(Filter(PieceType.KNIGHT)(1, -2))
@@ -6,8 +20,27 @@ console.log(Filter(PieceType.BISHOP)(-4, 4))
 console.log(Filter(PieceType.ROOK)(0, 5))
 console.log(Filter(PieceType.QUEEN)(2, 2))
 console.log(Filter(PieceType.KING)(1, -1)) */
-const foo = new Piece(Color.WHITE, PieceType.BISHOP)
-const bar = new Piece(Color.TRUE, PieceType.ROOK, 6, 6)
+
+/* const foo = new Piece(Color.WHITE, PieceType.BISHOP)
+const bar = new Piece(Color.TRUE, PieceType.ROOK, Rank[7], File.g)
 
 SelectPiece(foo)
-console.log(CheckMove([6, 6]))
+console.log(CheckMove([Rank[7], File.g])) */
+
+Board()
+
+SelectPiece(GetPiece(Coords('e2')))
+Move(Coords('e4'))
+Move(Coords('e5'))
+Move(Coords('e6'))
+Move(Coords('d7'))
+
+console.log(GetPiece(Coords('e8')))
+SelectPiece(GetPiece(Coords('e8')))
+Move(Coords('d7'))
+
+/* for(let i = 0; i < 8; i++) {
+	for(let j = 0; j < 8; j++) {
+		console.log(i, j, CheckMove([i, j]))
+	}
+} */
--- pce.js
+++ pce.js
@@ -1,6 +1,8 @@
-const PIECES = []
+var 
+PIECES = []
 
 var selected = -1
+var material_eval = 0
 
 const Color = {
 	NONE: -1,
@@ -18,26 +20,51 @@ const PieceType = {
 	KING: 5,
 }
 
+const Material = {
+	[PieceType.PAWN]: 1,
+	[PieceType.KNIGHT]: 3,
+	[PieceType.BISHOP]: 3,
+	[PieceType.ROOK]: 5,
+	[PieceType.QUEEN]: 9,
+}
+
+const Rank = {
+	1: 0,
+	2: 1,
+	3: 2,
+	4: 3,
+	5: 4,
+	6: 5,
+	7: 6,
+	8: 7,
+}
+
+const File = {
+	a: 0,
+	b: 1,
+	c: 2,
+	d: 3,
+	e: 4,
+	f: 5,
+	g: 6,
+	h: 7,
+}
+
 const Flags = {
 	NONE: -1,
 	CAPTURE: 0,
-	FIRST_PAWN: 1,
+	FIRST: 1,
 	CASTLE: 2,
 	QUEENSIDE_CASTLE: 3,
 	EN_PASSANT: 4,
 }
 
-const HasFlag = (flags, flag) => {
-	for(const fl of flags) {
-		if(fl === flag) { return true }
-	}
-	return false
-}
+const HasFlag = (flags, flag) => flags.indexOf(flag) !== -1
 
 const DefaultFilter = (rank, file) => rank < 8 && rank > -8 && file < 8 && file > -8 && !(rank === 0 && file === 0)
 
 const BaseFilters = {
-	[PieceType.PAWN]: (rank, file) => DefaultFilter(rank, file) && (rank === 1 && file === 0),
+	[PieceType.PAWN]: (rank, file) => false, // DefaultFilter(rank, file) && (rank === 1 && file === 0),
 	[PieceType.KNIGHT]: (rank, file) => DefaultFilter(rank, file) && ((Math.abs(rank) === 2 && Math.abs(file) === 1) || (Math.abs(rank) === 1 && Math.abs(file) === 2)),
 	[PieceType.BISHOP]: (rank, file) => DefaultFilter(rank, file) && (Math.abs(rank) === Math.abs(file)),
 	[PieceType.ROOK]: (rank, file) => DefaultFilter(rank, file) && (rank * file === 0),
@@ -55,12 +82,12 @@ const AdvancedOptionFilter = (filter, options) => (rank, file) => {
 
 const Filter = (type, flags = []) => {
 	if(type === PieceType.PAWN) {
-		const filter = []
+		let filter = []
 		filter.push([1, 0])
-		if(HasFlag(flags, Flags.CAPTURE) || HasFlag(flags, Flags.EN_PASSANT)) {
-			filter.push([1, 1], [1, -1])
-		} if(HasFlag(flags, Flags.FIRST_PAWN)) {
+		if(HasFlag(flags, Flags.FIRST)) {
 			filter.push([2, 0])
+		} else if(HasFlag(flags, Flags.CAPTURE) || HasFlag(flags, Flags.EN_PASSANT)) {
+			filter = [[1, 1], [1, -1]]
 		}
 		return AdvancedOptionFilter(BaseFilters[PieceType.PAWN], filter)
 	} else { return BaseFilters[type] }
@@ -96,19 +123,27 @@ const CheckDest = (piece, dest) => {
 	return [true, undefined]
 }
 
+const BlackConversion = (coords) => [-coords[0], -coords[1]]
+
+const Offset = (start, end) => [end[0] - start[0], end[1] - start[1]]
+
 const CheckMove = (dest) => {
 	const flags = []
 	const piece = PIECES[selected]
 
-	if(!CheckPath(piece, dest)) { return false }
+	if(piece.type !== PieceType.KNIGHT && !CheckPath(piece, dest)) { return false }
+	
+	if(!piece.moved) { flags.push(Flags.FIRST) }
 
 	const [dest_auth, capture_flag] = CheckDest(piece, dest)
 
 	if(!dest_auth) { return false }
 
 	if(capture_flag === 0) { flags.push(capture_flag) }
-	
-	return Filter(piece.type, flags)(...dest)
+
+	const offset = piece.color === Color.BLACK ? BlackConversion(Offset(piece.coords(), dest)) : Offset(piece.coords(), dest)
+
+	return [Filter(piece.type, flags)(...offset), flags]
 }
 
 const SelectPiece = (piece) => {
@@ -117,11 +152,83 @@ const SelectPiece = (piece) => {
 	}
 }
 
+const GetPiece = (coords) => {
+	for(const piece of PIECES) {
+		if(JSON.stringify(coords) === JSON.stringify(piece.coords())) { return piece }
+	}
+}
+
+const Board = () => {
+	new Piece(Color.WHITE, PieceType.ROOK, Rank[1], File.a)
+	new Piece(Color.WHITE, PieceType.KNIGHT, Rank[1], File.b)
+	new Piece(Color.WHITE, PieceType.BISHOP, Rank[1], File.c)
+	new Piece(Color.WHITE, PieceType.QUEEN, Rank[1], File.d)
+	new Piece(Color.WHITE, PieceType.KING, Rank[1], File.e)
+	new Piece(Color.WHITE, PieceType.BISHOP, Rank[1], File.f)
+	new Piece(Color.WHITE, PieceType.KNIGHT, Rank[1], File.g)
+	new Piece(Color.WHITE, PieceType.ROOK, Rank[1], File.h)
+
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.a)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.b)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.c)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.d)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.e)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.f)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.g)
+	new Piece(Color.WHITE, PieceType.PAWN, Rank[2], File.h)
+	
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.a)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.b)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.c)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.d)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.e)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.f)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.g)
+	new Piece(Color.BLACK, PieceType.PAWN, Rank[7], File.h)
+	
+	new Piece(Color.BLACK, PieceType.ROOK, Rank[8], File.a)
+	new Piece(Color.BLACK, PieceType.KNIGHT, Rank[8], File.b)
+	new Piece(Color.BLACK, PieceType.BISHOP, Rank[8], File.c)
+	new Piece(Color.BLACK, PieceType.QUEEN, Rank[8], File.d)
+	new Piece(Color.BLACK, PieceType.KING, Rank[8], File.e)
+	new Piece(Color.BLACK, PieceType.BISHOP, Rank[8], File.f)
+	new Piece(Color.BLACK, PieceType.KNIGHT, Rank[8], File.g)
+	new Piece(Color.BLACK, PieceType.ROOK, Rank[8], File.h)
+}
+
+const Coords = str =>  [str[1] - 1, ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].indexOf(str[0])]
+const Notations = coords => `${['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'][coords[1]]}${coords[0] + 1}`
+
+const TakePiece = (coords) => {
+	let tmp = selected
+	SelectPiece(GetPiece(coords))
+	if(selected < tmp) { tmp-- }
+	const select = PIECES[selected]
+	material_eval += Material[select.type] * select.color ? 1 : -1
+	PIECES = PIECES.slice(0, selected).concat(PIECES.slice(selected + 1, PIECES.length))
+	selected = tmp
+}
+
+const Move = (coords) => {
+	const move_check = CheckMove(coords)
+	if(move_check[0]) {
+		if(move_check[1].indexOf(0) !== -1) {
+			TakePiece(coords)
+		}
+		[PIECES[selected].rank, PIECES[selected].file] = coords
+		PIECES[selected].moved = true
+		console.log(`Moved to ${Notations(coords)} (eval: ${material_eval})`)
+	} else {
+		console.log(`ERROR: ${Notations(coords)} is an invalid move`)
+	}
+}
+
 class Piece {
 	color = Color.NONE
 	type = PieceType.NONE
 	rank = 0
 	file = 0
+	moved = false
 	
 	constructor(color, type, rank = 0, file = 0) {
 		this.color = color
@@ -139,8 +246,14 @@ module.exports = {
 	Color,
 	Piece,
 	PieceType,
+	Rank,
+	File,
 	Filter,
 	Flags,
 	CheckMove,
 	SelectPiece,
+	Board,
+	GetPiece,
+	Coords,
+	Move,
 }
